fba8e22e7732480a84a731bd2257713b16bc9210
Girijajethitech <girijashankarmohanta11@gmail.com>
2023-11-06 19:47:19 +0530

    dd

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,10 +6,10 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Frontend Developer",
+          "Web Developer",
+          "Web Designer",
+          "UI Developer",
         ],
         autoStart: true,
         loop: true,
